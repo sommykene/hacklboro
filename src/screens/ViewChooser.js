dc0dc6679e26c25f154e315022a816e04ed5c8db
KataLendvai <79164339+KataLendvai@users.noreply.github.com>
2021-03-13 15:37:26 +0000

    Added ViewChooser screen

---
+++ src/screens/ViewChooser.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { StyleSheet, View, Text, Button, Alert } from 'react-native'
+import { color } from 'react-native-reanimated'
+
+const ViewChooser = () => {
+    return(
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', paddingTop: 250}}>
+
+            <View style={styles.container}>
+            <Button
+                title="Guest"
+                color='#C4C4C4'
+                onPress={() => Alert.alert('Guest Button pressed')}
+              />
+            </View>
+
+            <View style={styles.container}>
+            <Button 
+                title="Student"
+                color='#C4C4C4'
+                onPress={() => Alert.alert('Student Button pressed')}
+              />
+            </View>
+
+            <View style={styles.container}>
+            <Button 
+               title="Teacher"
+               color='#C4C4C4'
+               onPress={() => Alert.alert('Teacher Button pressed')}
+             />
+            </View>
+           
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    
+    container: { 
+    borderRadius:20, 
+    padding:20,
+    width:'70%',
+    height: '40%',
+    alignContent: 'center',
+    fontSize: 40,
+    fontWeight: 'bold',
+ },
+})
+
+export default ViewChooser 
