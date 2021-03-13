8381bb0452d4f3b8a36d796bc95f332d0a638f7f
sommykene <32955277+sommykene@users.noreply.github.com>
2021-03-13 13:51:52 +0000

    updated video player

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import {Component} from 'react';
+import React, {useState} from 'react';
 import {WebView} from 'react-native-webview';
-import { StyleSheet, Text, View, Dimensions} from 'react-native';
-import Constant from 'expo-constants';
+import {StyleSheet, Text, View, Dimensions} from 'react-native';
 
-const VideoPlayer = (props) => {
-    return(
-        <View style = {{
-            flex: 1,
-            marginTop:Constant, statusBarHeight
+const VideoPlayer = ({route}) => {
+  const [videoID, setVideoID] = useState('');
+  React.useEffect(() => {
+    if (route.params?.videoId) {
+      setVideoID(route.params?.videoId);
+    }
+  }, [route.params?.videoId]);
+  return (
+    <View
+      style={{
+        flex: 1,
+        // marginTop: Constant,
+        // statusBarHeight,
+      }}>
+      <View
+        style={{
+          width: '100%',
+          height: 200,
         }}>
-            <View style = {{
-                width: "100%",
-                height: 200
-            }}> 
-            <WebView source={{ uri: `https://youtu.be//embed/${props.vidoeId}`}}/>
+        <WebView source={{uri: `https://www.youtube.com/embed/${videoID}`}} />
+      </View>
+      <Text
+        style={{
+          fontSize: 20,
+          //   width: Dimensions.get('screen').width - 50,
+          margin: 9,
+        }}
+        numberOfLines={2}
+        ellipsizeMode="tail">
+        Lesson 1
+      </Text>
+      <View style={{borderBottomwidth: 1}} />
+    </View>
+  );
+};
 
-            </View>
-            <Text style ={{
-                fontsize:20,
-                width: Dimensions.get("screen").width - 50,
-                margin: 9
-            }}
-            numberOfLines={2}
-            ellipsizeMode="tail"
-            > 
-            {props.title}
-            Lesson 1</Text>
-            <View
-            style = {{borderBottomwidth:1 }}/>
-        </View>
-    )
-}
+export default VideoPlayer;
