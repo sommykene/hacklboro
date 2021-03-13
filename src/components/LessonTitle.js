b95542a19ace3ddaed68c6940cc069cbe44f733e
KataLendvai <79164339+KataLendvai@users.noreply.github.com>
2021-03-13 10:34:59 +0000

    Update LessonTitle.js

--- src/components/LessonTitle.js
+++ src/components/LessonTitle.js
@@ -3,19 +3,39 @@ import { Image, StyleSheet, Text, View } from 'react-native'
 
 const arrow = require('../images/arrow.png');
 
-const LessonTitle = () => {
-    return (<View style={styles.container}>
-        <Text style={{fontWeight:'bold'}}> 1. this is lesson 1 </Text>
+const LessonTitle = ({title,locked}) => {
+    return (
+    <View style={{height: 55}}> 
+    <View style={styles.container}>
+        <Text style={{fontWeight:'bold'}}> {title} </Text>
         <Image source={arrow} />
-    </View>)
+        </View>
+
+    {locked ===true ?
+        //this will appear if lesson is locked
+        (<View style={styles.overlay}></View>
+    ) : (
+        null
+    )}
+    </View>
+    )
 }
 
 const styles = StyleSheet.create({
     container: {backgroundColor:'#E5E5E5', 
     borderRadius: 10, 
     padding:15, 
+    height: '100%',
     flexDirection:'row',
-    justifyContent: 'space-between', }
+    justifyContent: 'space-between', },
+
+    overlay: {
+        position: 'absolute',
+        height: '100%',
+        width: '100%',
+        backgroundColor: 'rgba(12,23,36,0.5)',
+        borderRadius: 10,
+    }
 })
 
 export default LessonTitle
