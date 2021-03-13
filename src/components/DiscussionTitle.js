4ed20fc3e1c4ce45e444958a375ba35bd89122ca
KataLendvai <79164339+KataLendvai@users.noreply.github.com>
2021-03-13 22:58:09 +0000

    comments

--- src/components/DiscussionTitle.js
+++ src/components/DiscussionTitle.js
@@ -2,29 +2,35 @@ import React from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
 import Spacer from '../utils/Spacer';
 
+{/*a discussion needs a question and answer to start*/}
 const DiscussionTitle = ({question, answer}) => {
   return (
     <View style={styles.container}>
+      {/*discussion question*/}
       <Text style={{fontWeight: 'bold', fontSize: 18}}>{question}</Text>
-
+      {/*discussion answer*/}
       <Text>
         <Text style={{fontWeight: 'bold'}}>Jane: </Text>
         {answer}
       </Text>
+
       <Spacer />
+
       <View
         style={{
           flexDirection: 'row',
           justifyContent: 'space-between',
           paddingHorizontal: 10,
         }}>
+      {/*discussion responses*/}
         <Text>4 responses</Text>
+      {/*view discussion link*/}
         <Text style={{color: '#1792EB'}}>View Discussion</Text>
       </View>
     </View>
   );
 };
-
+{/*style sheet*/}
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#E5E5E5',
